72d49d29c1389c4afcd2b6f7a524e158353b0d99
andriih <andrii.hnatyshyn@gmail.com>
2017-10-18 14:15:37 +0300

    router v3 install

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import Toolbar from './components/Toolbar';
 import Content from './components/Content';
 import Home from './pages/Home';
 
+
+
 class App extends Component {
     render() {
         return (
             <div className="app">
-                <Toolbar />
-                
-                <Content>
-                    <Home />
-                </Content>
+                    <Toolbar />
+                    <Content>
+                        <Home />
+                    </Content>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route , hashHistory } from 'react-router'
 
 import App from './App';
+import About from'./pages/About'
+import Books from'./pages/Books'
+import NotFound from'./pages/NotFound'
 
 import 'material-components-web/dist/material-components-web.css';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+				<Router history={hashHistory}>
+					<div>
+						<Route path="/" component={App} />
+						<Route path="about" component={About} />
+						<Route path="books" component={Books} />
+						<Route path="*" component={NotFound} />	
+
+					</div>
+				</Router>,
+			document.getElementById('root')
+	)
